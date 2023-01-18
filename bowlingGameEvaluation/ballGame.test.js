ee88ddd15b44d3b429d381b422cfa88e9f23bace
Naman Bhatia <Naman_Bhatia@mckinsey.com>
2023-01-18 13:27:18 +0530

    chores: test file

--- bowlingGameEvaluation/ballGame.test.js
+++ bowlingGameEvaluation/ballGame.test.js
@@ -1,10 +1,10 @@
-const {scoreCalculator}=require('./ballGame')
+const {getBestScore}=require('./ballGame')
 
 describe('Bowling Game Scorer', ()=>{
     describe('Calculates score of a 10 frame bowling game', ()=>{
         it('should provide score when number of frames is 10',()=>{
-            const result=scoreCalculator([10, 5, 5, 9, 0]);
-            expect(result[3]).toEqual(20);
+            const result=getBestScore([10, 5, 5, 9, 0]);
+            expect(result).toEqual(48);
         });
         
     });
