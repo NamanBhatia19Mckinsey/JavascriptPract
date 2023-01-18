957175663abe0a33ee1ff3eb561e6db2310ef3c7
Naman Bhatia <Naman_Bhatia@mckinsey.com>
2023-01-18 12:45:01 +0530

    chores: evaluation1 bowling game

---
+++ bowlingGameEvaluation/ballGame.js
@@ -0,0 +1,40 @@
+const getBestScore = (Games) => {
+    let bestScore = -1;
+    Games.forEach((game) => {
+        let currScore = getScore(game);
+        if(currScore > bestScore) {
+            bestScore = currScore;
+        }
+    });
+    return bestScore;
+}
+
+const getScore = (Rolls) => {
+    let frames = [];
+    let index =0;
+    for(index =0;index<=Rolls.length;index+=2) {
+        let first = Rolls[index];
+        let second = Rolls[index+1];
+        if(first === 10 || first + second == 10) {
+            frames.push([first,second,Rolls[index+2]]);
+        } else if(first + second < 10) {
+            frames.push([first,second]);
+        }
+    }
+    if(Rolls[index] === 10 || Rolls[index + 1] + Rolls[index] === 10) {
+        //spare
+        frames.push([Rolls[index],Rolls[index+1],Rolls[index+2]]);
+    } else if(Rolls[index] + Rolls[index+1] < 10){
+        //open frame
+        frames.push([Rolls[index],Rolls[index+1]]);
+    }
+    let scores = 0;
+    frames.forEach((frame) => {
+        frame.forEach((pins)=> {
+            scores += pins;
+        });
+    });
+    return scores;
+}
+
+console.log(getScore([10, 5, 5, 9, 0]));
